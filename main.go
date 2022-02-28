dda30419d051f0aa8898a3d74ef16512f38ef2b4
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-28 21:10:23 +0700

    set: functions

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string)(int, string){
+	defer fmt.Println("I'm done")
+	return len(name), strings.ToUpper(name)
+}
+
+func lenAndUpperV2(name string)(length int, uppsercase string){
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppsercase = strings.ToUpper(name)
+	return 
+}
+
+func multiply(a int,b int) int{
+	return a*b
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main(){
-	fmt.Println(
-		'Hello world',
-	)
-}
+	totlalLength, upperName := lenAndUpper("hello")
+	totlalLengthV2, upperNameV2 := lenAndUpperV2("hello")
+	name := "mj"
+	name = "hannah"
+	fmt.Println(name)
+	fmt.Println(multiply(4,5))
+	fmt.Println(totlalLength,upperName)
+	fmt.Println(totlalLengthV2,upperNameV2)
+	repeatMe("tomato", "apple", "orange", "banana")
+} 

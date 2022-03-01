2c059d993da78028469f8cd1625514159c01b068
plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-01 21:59:19 +0700

    set: methods

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,6 +1,6 @@
 package accounts
 
-import "fmt"
+import "errors"
 
 // Account struct
 type Account struct {
@@ -8,11 +8,29 @@ type Account struct {
 	balance int
 }
 
+var errNoMoney = errors.New("Can't withdraw")
+
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
-	fmt.Println("owner", owner)
 	account := Account{owner: owner, balance: 0}
-	fmt.Println("account", account)
-	fmt.Println("&account", &account)
 	return &account	// struct의 address는 이렇게 생겼다. &{mj 0}
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -9,5 +9,12 @@ import (
 
 func main() {
 	account := accounts.NewAccount("mj")
-	fmt.Println((account))
+	fmt.Println(account)
+	account.Deposit(100)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }

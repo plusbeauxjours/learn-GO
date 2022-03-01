a3754a81529281d64d2ae9ea69fc748c4e3f6450
plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-01 22:21:36 +0700

    set: String method

--- main.go
+++ main.go
@@ -11,10 +11,5 @@ func main() {
 	account := accounts.NewAccount("mj")
 	fmt.Println(account)
 	account.Deposit(100)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }

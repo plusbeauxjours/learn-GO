8fc029a366bdda0f41198c682fc7686c10af54e8
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-03 22:16:02 +0700

    set: update  & delete method

--- main.go
+++ main.go
@@ -9,18 +9,17 @@ import (
 func main() {
 	dictionary := mydict.Dictionary{}
 
-	word := "hello"
-	definition := "Greeting"
+	baseWord := "hello"
 
-	err := dictionary.Add(word, definition)
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+
+	word, err := dictionary.Search(baseWord)
+	
 	if err != nil {
 		fmt.Println(err)
-	}
-	
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "definition:", hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
+	} else {
+		fmt.Println(word)
 	}
 }

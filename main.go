49823fafc267417b71d7fb675e0d921f274e0815
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-03 22:35:25 +0700

    set: println

--- main.go
+++ main.go
@@ -12,13 +12,27 @@ func main() {
 	baseWord := "hello"
 
 	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-
 	word, err := dictionary.Search(baseWord)
-	
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
+	}
+	
+	dictionary.Update(baseWord, "Second")
+	word, err2 := dictionary.Search(baseWord)
+
+	if err2 != nil {
+		fmt.Println(err2)
+	} else {
+		fmt.Println(word)
+	}
+
+	dictionary.Delete(baseWord)
+	word, err3 := dictionary.Search(baseWord)
+
+	if err3 != nil {
+		fmt.Println(err3)
 	} else {
 		fmt.Println(word)
 	}
